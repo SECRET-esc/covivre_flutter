767108d6e29270cea402a8b1ead62f511f018437
v.kucher <v.kucher@lucky-labs.com>
2020-11-07 23:40:40 +0200

    working with ForegroundService

--- android/app/src/main/kotlin/com/example/covivre/BleModule.kt
+++ android/app/src/main/kotlin/com/example/covivre/BleModule.kt
@@ -2,21 +2,26 @@ package com.example.covivre
 
 
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanResult
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.*
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
 import android.os.Handler
-import android.os.PersistableBundle
+import android.os.ParcelUuid
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.reflect.Method
+import java.util.*
 
 
 class BleModule() : AppCompatActivity() {
     private val TAG : String = "debug"
+
+
+    val random: Random = Random()
+
     lateinit var btLeScanner: BluetoothLeScanner
     lateinit var handler: Handler
     private lateinit var btAdapter: BluetoothAdapter
@@ -26,6 +31,19 @@ class BleModule() : AppCompatActivity() {
     var scanPeriod: Long = 5000
     var strengthThreshold = -60
     private lateinit var context: ForegroundService
+    var btManager: BluetoothManager? = null
+    var advertiser: BluetoothLeAdvertiser? = null
+    var idChannel = "my_channel_01"
+    private val uuid = UUID.fromString("10000000-0000-0000-0000-000000000001")
+
+
+    companion object {
+        lateinit var instance: BleModule
+    }
+
+    init {
+        instance = this
+    }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun scanLeDevice() {
@@ -45,13 +63,15 @@ class BleModule() : AppCompatActivity() {
     private val callBack: ScanCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     object : ScanCallback(){
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-        override fun onScanResult(callbackType:Int, result: ScanResult) {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
             val name = result.device.name
+            val uuids = result.device.uuids
             if (name!==null) {
                 getResult(name)
                 context.sendResult(name)
             }
+            Log.d(TAG, "Device name:" + name + "rssi: " + result.getRssi() + "uuids: " + uuids.toString());
             btLeScanner.startScan(callBackStop)
 
         }
@@ -59,12 +79,27 @@ class BleModule() : AppCompatActivity() {
     private val callBackStop: ScanCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     object : ScanCallback(){
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-        override fun onScanResult(callbackType:Int, result: ScanResult) {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
             context.sendResult("scanning was stopped! - $result")
             Log.d(TAG, "scanning was stopped!$result")
         }
     }
+
+    private val mAdvertiseCallback: AdvertiseCallback = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+        object : AdvertiseCallback() {
+            override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+                Log.i("App", "LE Advertise Started.")
+            }
+
+            override fun onStartFailure(errorCode: Int) {
+                Log.w("App", "LE Advertise Failed: $errorCode")
+            }
+        }
+    } else {
+        TODO("VERSION.SDK_INT < LOLLIPOP")
+    }
+
 
     private fun isAboveMarshmallow(): Boolean {
         Log.d(TAG, "In isAboveMarshmallow")
@@ -72,10 +107,9 @@ class BleModule() : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    fun getResult(name : String){
+    fun getResult(name: String){
         Log.d(TAG, "In result")
     }
-
 
 
     fun startScan(context: ForegroundService){
@@ -96,18 +130,64 @@ class BleModule() : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     private fun initBLEModule() {
         btAdapter = BluetoothAdapter.getDefaultAdapter()
+        btLeScanner = btAdapter.getBluetoothLeScanner();
+        advertiser = btAdapter.getBluetoothLeAdvertiser();
+        Log.d(TAG, "Start service");
         btLeScanner = btAdapter.bluetoothLeScanner
         Log.d(TAG, "In initBLEModule")
 // BLE initialization
         if (!btAdapter.isEnabled) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            MainActivity.instance.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
         if (btAdapter.isEnabled) {
             scanLeDevice()
+            startAdvertising()
         }
     }
 
+    private fun startAdvertising() {
+        if (advertiser == null) {
+            Log.d("App", "Failed to create advertiser")
+            return
+        }
+        val settings = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            AdvertiseSettings.Builder()
+                    .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                    .setConnectable(true)
+                    .setTimeout(0)
+                    .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                    .build()
+        } else {
+            TODO("VERSION.SDK_INT < LOLLIPOP")
+        }
+        val data = AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .setIncludeTxPowerLevel(false)
+                .addServiceUuid(ParcelUuid(uuid))
+                .build()
+        advertiser!!
+                .startAdvertising(settings, data, mAdvertiseCallback)
+    }
+
+
+    fun turnOnDiscoverable() {
+        if (btAdapter.scanMode != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Log.d(ContentValues.TAG, "Restart discoverable")
+            val method: Method
+            try {
+                method = btAdapter.javaClass.getMethod("setScanMode", Int::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+                method.invoke(btAdapter, BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE, 400)
+                Log.e("invoke", "method invoke successfully")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun cancelDiscovery(){
+        btAdapter.cancelDiscovery()
+    }
 
 
 }
--- android/app/src/main/kotlin/com/example/covivre/ForegroundService.kt
+++ android/app/src/main/kotlin/com/example/covivre/ForegroundService.kt
@@ -2,11 +2,10 @@ package com.example.covivre
 
 import android.R
 import android.app.*
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.*
 import android.content.ContentValues.TAG
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.BatteryManager
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
@@ -15,13 +14,27 @@ import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import java.lang.Math.random
 
 
 class ForegroundService : Service() {
     private val CHANNEL_ID: String = "my_chanel"
-    private val CHANNEL = "samples.flutter.dev/battery"
+    private val CHANNEL = "covivre/scan"
     private val myBinder = MyLocalBinder()
     private lateinit var methodChannel: MethodChannel
+    val bleModule = BleModule()
+    var s: MutableList<String>? = null
+    var sick = 0
+    private val DELAY = 2
+    private var signal = 100.0
+
+    companion object {
+        lateinit var instance: ForegroundService
+    }
+
+    init {
+        instance = this
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -33,16 +46,9 @@ class ForegroundService : Service() {
         this.methodChannel.setMethodCallHandler {
             // Note: this method is invoked on the main thread.
             call, result ->
-            if (call.method == "getBatteryLevel") {
-                val batteryLevel = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    getBatteryLevel()
-                } else {
-                    TODO("VERSION.SDK_INT < LOLLIPOP")
-                }
-                val bleModule = BleModule()
-                bleModule.startScan(this)
-                if (batteryLevel != -1) {
-                    result.success(batteryLevel)
+            if (call.method == "startScan") {
+                if (startScan() != -1) {
+                    result.success(0)
                 } else {
                     result.error("UNAVAILABLE", "Battery level not available.", null)
                 }
@@ -52,7 +58,7 @@ class ForegroundService : Service() {
         }
     }
 
-    fun sendResult(arguments: String){
+    fun sendResult(arguments: String) {
         print("sendResult = $arguments")
         if (this::methodChannel.isInitialized) {
             this.methodChannel.invokeMethod("result", arguments, object : MethodChannel.Result {
@@ -68,23 +74,15 @@ class ForegroundService : Service() {
                     print("notImplemented")
                 }
             })
-        }else{
+        } else {
             print("this::methodChannel.isInitialized not init")
         }
     }
 
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun getBatteryLevel(): Int {
-        val batteryLevel: Int
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val batteryManager = getSystemService(BATTERY_SERVICE) as BatteryManager
-            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-        } else {
-            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-        }
-        return batteryLevel
+    private fun startScan(): Int {
+        bleModule.startScan(this)
+        return 0
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
@@ -104,16 +102,18 @@ class ForegroundService : Service() {
         Log.d(TAG, "In onStartCommand")
         return START_NOT_STICKY
     }
+
     fun getService(): ForegroundService? {
         // Return this instance of MyService so clients can call public methods
         return this@ForegroundService
     }
+
     override fun onBind(intent: Intent): IBinder? {
         return myBinder
     }
 
     inner class MyLocalBinder : Binder() {
-        fun getService() : ForegroundService {
+        fun getService(): ForegroundService {
             return this@ForegroundService
         }
 
@@ -126,5 +126,112 @@ class ForegroundService : Service() {
                 "Foreground Service Channel",
                 NotificationManager.IMPORTANCE_DEFAULT)
         getSystemService(NotificationManager::class.java).createNotificationChannel(serviceChannel)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun ShowNotification() {
+        Log.d(TAG, "In notify func")
+        createNotificationChannel()
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                .setContentTitle("You in dangerous!")
+                .setContentText("Close to you have a ill person!")
+                .setSmallIcon(R.drawable.alert_dark_frame)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        val notification: Notification = builder.build()
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val num = random().toInt() + 1
+        notificationManager.notify(num, notification)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(mBroadcastReceiver1)
+        unregisterReceiver(mBroadcastReceiver2)
+        unregisterReceiver(mBroadcastReceiver3)
+        Log.d(TAG, "stop scanning!")
+        bleModule.cancelDiscovery()
+        stopSelf()
+    }
+
+    private val mBroadcastReceiver1: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            val action = intent.action
+            if (action == BluetoothAdapter.ACTION_DISCOVERY_FINISHED) {
+                Log.d(TAG, "Scan was stopped!")
+                s?.clear()
+                sick = 0
+                s?.add("0x101010xmx909022x235ff5")
+                val intent2 = Intent()
+                intent2.putExtra("FindSick", 3)
+                sendBroadcast(intent2)
+                try {
+                    Thread.sleep(1000 * DELAY.toLong())
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                unregisterReceiver(this)
+//                startDiscovering()
+            }
+        }
+    }
+    private val mBroadcastReceiver2: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            val action = intent.action
+            if (action == BluetoothAdapter.ACTION_DISCOVERY_STARTED) {
+                Log.d(TAG, "Scan was STARTED!")
+            }
+            bleModule.turnOnDiscoverable()
+        }
+    }
+
+
+
+// Device scan callback.
+private val mBroadcastReceiver3: BroadcastReceiver = object : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent1: Intent) {
+        val action = intent1.action
+        Log.d(TAG, "onReceive: ACTION FOUND.")
+        val rssi = intent1.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE).toInt()
+        Log.d(TAG, "RSSI:$rssi")
+        if (action == BluetoothDevice.ACTION_FOUND) {
+            val device = intent1.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+            val name = if (device?.name != null) device.name else "none"
+            Log.d(TAG, "onReceive: " + name + ": " + device?.address)
+            Log.d(TAG, name)
+            if (name !== "none") {
+                if (name.length > 3) {
+                    if (name.substring(name.length - 3) == "x34") {
+                        val device_name: Boolean = s?.contains(name)!!
+                        if (!device_name) {
+                            if (rssi > -69) {
+                                //TODO: write logic
+                            } else {
+                                val meters: Double = getMeters(rssi)
+                                if (meters < signal) {
+                                    signal = meters
+                                } else {
+                                    Log.i(TAG, "Signal val:" + signal.toString() + "Meters:" + meters)
+                                }
+                            }
+                            sick++
+                            Log.d(TAG, "Sick:$sick")
+                            s!!.add(name)
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Name lower 3")
+                }
+                Log.d(TAG, """Device Name: ${device?.name} Address: ${device?.address}
+""")
+            }
+        }
+    }
+}
+
+    fun getMeters(rssi: Int): Double {
+        Log.d(TAG, "Calculate meters...")
+        val i = Math.pow(10.0, (-69.0 - rssi) / (10 * 2))
+        Log.d(TAG, "Meters = $i")
+        return i
     }
 }
